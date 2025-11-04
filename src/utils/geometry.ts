9508d8193f9746ec6e8fabd64c03a2a02bb5cc79
gbsr <anders.jakobsen.hofsten@gmail.com>
2025-11-04 17:57:58 +0100

    refactor geometry

--- src/utils/geometry.ts
+++ src/utils/geometry.ts
@@ -1,3 +1,5 @@
+import type { Point } from "../interface/point"
+import type { Rect } from "../interface/rect"
 import getMousePosInCanvas from "./pointer"
 
 export default function checkIfInsideRect(canvas: HTMLCanvasElement, e: PointerEvent, rectPos: { x: number; y: number; width: number; height: number }) {
@@ -15,4 +17,13 @@ export default function checkIfInsideRect(canvas: HTMLCanvasElement, e: PointerE
     console.log('Clicked inside final selection');
     return true;
   }
+}
+
+export function pointInsideRect(mouse: Point, finalSelection: Rect) {
+  return (
+    mouse.x >= finalSelection.x &&
+    mouse.x <= finalSelection.x + finalSelection.width &&
+    mouse.y >= finalSelection.y &&
+    mouse.y <= finalSelection.y + finalSelection.height
+  );
 }
